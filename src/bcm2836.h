740b21c2dc12331f1f23e45856287497d5d33efa
David Wright <david.wright@bluewin.ch>
2015-07-28 13:25:00 +0200

    + Initial commit.

---
+++ src/bcm2836.h
@@ -0,0 +1,22 @@
+/*
+ * bcm2836.h
+ *
+ *  Created on: 28 Jul 2015
+ *      Author: David Wright
+ */
+
+#ifndef SRC_BCM2836_H_
+#define SRC_BCM2836_H_
+
+#ifndef SRC_BCM2836_H_
+#define SRC_BCM2836_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SRC_BCM2836_H_ */
